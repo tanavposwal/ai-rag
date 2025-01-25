e8ca337180d8547eb88742855cc092bb40522ec0
tanavposwal <tanavposwal939@gmail.com>
2025-01-25 12:01:24 +0530

    remove pdf

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-from dotenv import load_dotenv
 import os
-import pandas as pd
-from llama_index.query_engine import PandasQueryEngine
 from prompts import new_prompt, instruction_str, context
 from note_engine import note_engine
 from llama_index.tools import QueryEngineTool, ToolMetadata
 from llama_index.agent import ReActAgent
 from llama_index.llms import Ollama
-from pdf import india_engine
-
-load_dotenv()
-
-population_path = os.path.join("data", "population.csv")
-population_df = pd.read_csv(population_path)
-
-population_query_engine = PandasQueryEngine(
-    df=population_df, verbose=True, instruction_str=instruction_str
-)
-population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+from pdf import india_engine, deforestation_engine
 
 tools = [
     note_engine,
     QueryEngineTool(
-        query_engine=population_query_engine,
-        metadata=ToolMetadata(
-            name="population_data",
-            description="this gives information at the world population and demographics",
-        ),
-    ),
-    QueryEngineTool(
-        query_engine=india_engine,
+        query_engine=deforestation_engine,
         metadata=ToolMetadata(
             name="india_data",
             description="this gives detailed information about climate in india the country",
